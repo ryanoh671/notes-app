22b428c4e07d56c62aa4211017abb0144325738f
ryanoh671 <ryanoh671@gmail.com>
2023-08-16 13:11:05 -0700

    auth page and app.jsx routes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,12 +9,20 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav>
-      <Link to="/orders">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
-      &nbsp;&nbsp;
-      <span>Welcome, {user.name}</span>
-      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
+      { user ?
+      <>
+        <Link to="/orders">Order History</Link>
+        &nbsp; | &nbsp;
+        <Link to="/orders/new">New Order</Link>
+        &nbsp;&nbsp;
+        <span>Welcome, {user.name}</span>
+        &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
+      </>
+      :
+      <span>
+        <Link to="/auth">Login / Sign Up</Link>
+      </span>
+      }
     </nav>
   );
 }
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,16 +2,22 @@ import { useState } from 'react';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
 
-export default function AuthPage({ setUser }) {
+export default function AuthPage({ user, setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
   return (
     <main>
-      <h1>AuthPage</h1>
-      <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
-      { showSignUp ?
-          <SignUpForm setUser={setUser} />
-          :
-          <LoginForm setUser={setUser} />
+      { user ?
+      <h1></h1>
+      :
+      <>
+        <h1>AuthPage</h1>
+        <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
+        { showSignUp ?
+            <SignUpForm setUser={setUser} />
+            :
+            <LoginForm setUser={setUser} />
+        }
+      </>
       }
     </main>
   );
