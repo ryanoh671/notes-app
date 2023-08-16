e483c1a1c13d939132e14b2b851acc7524a2a136
ryanoh671 <ryanoh671@gmail.com>
2023-08-16 13:46:13 -0700

    add navigate on login button

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,8 +11,6 @@ export default function NavBar({ user, setUser }) {
     <nav>
       { user ?
       <>
-        <Link to="/orders">Order History</Link>
-        &nbsp; | &nbsp;
         <Link to="/orders/new">New Order</Link>
         &nbsp;&nbsp;
         <span>Welcome, {user.name}</span>
